068f1352c18f5329e387ee16fd753f5d9a6d73f9
HanJing1027 <#mail>
2025-02-15 02:29:44 +0800

    匯入題庫

---
+++ js/questions.js
@@ -0,0 +1,643 @@
+export const questions = [
+  {
+    category: "國文",
+    questions: [
+      {
+        question: "下列哪個詞語形容「很快樂」的心情？",
+        options: ["愁眉苦臉", "眉開眼笑", "悶悶不樂", "淚流滿面"],
+        correctAnswer: 1,
+      },
+      {
+        question: "「ㄅㄆㄇㄈ」中的「ㄇ」發音與下列哪個字相同？",
+        options: ["媽", "爸", "阿", "哥"],
+        correctAnswer: 0,
+      },
+      {
+        question:
+          "「窗外的麻雀，在電線桿上多嘴」，這句歌詞中的「多嘴」是什麼意思？",
+        options: ["說話很多", "吃很多東西", "嘴巴很大", "有很多嘴巴"],
+        correctAnswer: 0,
+      },
+      {
+        question: "「一年之計在於春」，這句話的意思是什麼？",
+        options: [
+          "春天很舒服",
+          "一年到頭都要計畫",
+          "一年的開始最重要",
+          "春天要去旅行",
+        ],
+        correctAnswer: 2,
+      },
+      {
+        question: "下列哪個字的部首是「心」？",
+        options: ["想", "看", "走", "飛"],
+        correctAnswer: 0,
+      },
+      {
+        question: "「每逢佳節倍思親」的「佳節」指的是什麼？",
+        options: ["平常日子", "不開心的日子", "節慶日子", "下雨天"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「與其」的後面應該接什麼？",
+        options: ["不如", "所以", "因此", "但是"],
+        correctAnswer: 0,
+      },
+      {
+        question: "下列哪個詞語形容「很快」？",
+        options: ["慢吞吞", "磨磨蹭蹭", "一步一步", "飛快"],
+        correctAnswer: 3,
+      },
+      {
+        question: "「春、夏、秋、冬」屬於什麼詞？",
+        options: ["象聲詞", "時序詞", "顏色詞", "數量詞"],
+        correctAnswer: 1,
+      },
+      {
+        question: "「小明寫作業寫得很認真」中的「得」字用法正確嗎？",
+        options: ["正確", "應改為的", "應改為地", "應該刪除"],
+        correctAnswer: 0,
+      },
+      {
+        question: "下列哪個不是反義詞？",
+        options: ["大小", "多少", "高矮", "天地"],
+        correctAnswer: 3,
+      },
+      {
+        question: "「輕聲細語」的反義詞是什麼？",
+        options: ["大聲小氣", "粗聲大氣", "輕言細語", "悄聲細語"],
+        correctAnswer: 1,
+      },
+      {
+        question: "「好」字可以組成下列哪個詞語？",
+        options: ["好天", "好氣", "好學", "好雨"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「竹子抽新芽」的「抽」字意思是什麼？",
+        options: ["拔出", "生長", "收縮", "抽取"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個詞語形容人很聰明？",
+        options: ["目中無人", "一竅不通", "聰明伶俐", "粗心大意"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「聞雞起舞」這個成語說的是誰？",
+        options: ["祖逖", "岳飛", "李白", "孔子"],
+        correctAnswer: 0,
+      },
+      {
+        question: "「謝謝」的「謝」字部首是什麼？",
+        options: ["言字旁", "心字旁", "手字旁", "竹字旁"],
+        correctAnswer: 0,
+      },
+      {
+        question: "「春眠不覺曉」的下一句是什麼？",
+        options: ["春色滿園關不住", "處處聞啼鳥", "萬物生機勃勃", "春風吹又生"],
+        correctAnswer: 1,
+      },
+      {
+        question: "「好好讀書」中的「好好」是什麼意思？",
+        options: ["很好的樣子", "認真地", "美好", "善良"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個詞語形容天氣很熱？",
+        options: ["寒冷徹骨", "涼爽宜人", "秋高氣爽", "炎炎夏日"],
+        correctAnswer: 3,
+      },
+      {
+        question: "「他寫字寫得很漂亮」中的「寫得」可以改成什麼？",
+        options: ["寫的", "寫地", "寫著", "都可以"],
+        correctAnswer: 0,
+      },
+      {
+        question: "「明天小明要去遠足」是什麼句子？",
+        options: ["疑問句", "感嘆句", "陳述句", "祈使句"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「我們要愛護地球」是什麼句子？",
+        options: ["陳述句", "祈使句", "感嘆句", "疑問句"],
+        correctAnswer: 0,
+      },
+      {
+        question: "下列哪個詞語形容人很懶惰？",
+        options: ["勤奮好學", "廢寢忘食", "遊手好閒", "專心致志"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「一日之計在於晨」的「晨」是什麼意思？",
+        options: ["黃昏", "中午", "早上", "晚上"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「望」可以加上哪個部首變成新字？",
+        options: ["扌", "忄", "氵", "艹"],
+        correctAnswer: 0,
+      },
+      {
+        question: "下列哪個不是四字成語？",
+        options: ["守株待兔", "亡羊補牢", "一心一意", "快樂"],
+        correctAnswer: 3,
+      },
+      {
+        question: "「小明，快去寫功課！」是什麼句子？",
+        options: ["感嘆句", "疑問句", "祈使句", "陳述句"],
+        correctAnswer: 2,
+      },
+      {
+        question: "「這件事情真有趣啊！」是什麼句子？",
+        options: ["陳述句", "疑問句", "祈使句", "感嘆句"],
+        correctAnswer: 3,
+      },
+      {
+        question: "下列哪個詞語形容人很忙碌？",
+        options: ["悠閒自在", "無所事事", "忙忙碌碌", "慢條斯理"],
+        correctAnswer: 2,
+      },
+    ],
+  },
+  {
+    category: "數學",
+    questions: [
+      {
+        question: "25 + 18 = ?",
+        options: ["42", "43", "44", "45"],
+        correctAnswer: 1,
+      },
+      {
+        question: "一個正方形有幾個角？",
+        options: ["3個", "4個", "5個", "6個"],
+        correctAnswer: 1,
+      },
+      {
+        question: "小美有 45 元，買了一支 12 元的鉛筆，還剩多少錢？",
+        options: ["32元", "33元", "34元", "35元"],
+        correctAnswer: 1,
+      },
+      {
+        question: "2 × 6 × 4 = ?",
+        options: ["36", "48", "52", "56"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個數是奇數？",
+        options: ["12", "24", "35", "48"],
+        correctAnswer: 2,
+      },
+      {
+        question: "360度除以4等於多少度？",
+        options: ["80度", "90度", "100度", "120度"],
+        correctAnswer: 1,
+      },
+      {
+        question: "一公尺等於幾公分？",
+        options: ["10公分", "100公分", "1000公分", "10000公分"],
+        correctAnswer: 1,
+      },
+      {
+        question: "一小時等於幾分鐘？",
+        options: ["30分鐘", "45分鐘", "60分鐘", "90分鐘"],
+        correctAnswer: 2,
+      },
+      {
+        question: "圓形有幾個角？",
+        options: ["0個", "1個", "2個", "3個"],
+        correctAnswer: 0,
+      },
+      {
+        question: "15 ÷ 3 = ?",
+        options: ["3", "4", "5", "6"],
+        correctAnswer: 2,
+      },
+      {
+        question: "哪一個圖形有三個角？",
+        options: ["正方形", "圓形", "三角形", "五角形"],
+        correctAnswer: 2,
+      },
+      {
+        question: "88 - 23 = ?",
+        options: ["55", "65", "75", "85"],
+        correctAnswer: 0,
+      },
+      {
+        question: "一星期有幾天？",
+        options: ["5天", "6天", "7天", "8天"],
+        correctAnswer: 2,
+      },
+      {
+        question: "下列哪個數是偶數？",
+        options: ["15", "27", "36", "43"],
+        correctAnswer: 2,
+      },
+      {
+        question: "1公斤等於幾公克？",
+        options: ["10公克", "100公克", "1000公克", "10000公克"],
+        correctAnswer: 2,
+      },
+      {
+        question: "時鐘的時針從3點轉到6點轉了幾度？",
+        options: ["45度", "60度", "90度", "120度"],
+        correctAnswer: 2,
+      },
+      {
+        question: "長方形有幾條邊？",
+        options: ["3條", "4條", "5條", "6條"],
+        correctAnswer: 1,
+      },
+      {
+        question: "720 ÷ 9 = ?",
+        options: ["70", "80", "90", "100"],
+        correctAnswer: 1,
+      },
+      {
+        question: "一個籃子裡有24顆蘋果，平分給3個人，每人可以得到幾顆？",
+        options: ["6顆", "8顆", "10顆", "12顆"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個數最大？",
+        options: ["25", "52", "35", "53"],
+        correctAnswer: 3,
+      },
+      {
+        question: "小華存了145元，花掉89元買文具，還剩多少錢？",
+        options: ["54元", "56元", "58元", "60元"],
+        correctAnswer: 1,
+      },
+      {
+        question: "三角形的內角和是多少度？",
+        options: ["90度", "120度", "180度", "360度"],
+        correctAnswer: 2,
+      },
+      {
+        question: "9 × 7 = ?",
+        options: ["56", "63", "72", "81"],
+        correctAnswer: 1,
+      },
+      {
+        question: "哪一個數字可以被2和3整除？",
+        options: ["14", "15", "16", "18"],
+        correctAnswer: 3,
+      },
+      {
+        question: "一個梯形有幾個頂點？",
+        options: ["3個", "4個", "5個", "6個"],
+        correctAnswer: 1,
+      },
+      {
+        question: "450 - 276 = ?",
+        options: ["174", "184", "194", "204"],
+        correctAnswer: 0,
+      },
+      {
+        question: "32 × 5 = ?",
+        options: ["150", "160", "170", "180"],
+        correctAnswer: 1,
+      },
+      {
+        question: "1元可以換幾個5角？",
+        options: ["1個", "2個", "3個", "4個"],
+        correctAnswer: 1,
+      },
+      {
+        question: "小明今年8歲，他的妹妹比他小3歲，他妹妹幾歲？",
+        options: ["3歲", "4歲", "5歲", "6歲"],
+        correctAnswer: 2,
+      },
+      {
+        question: "一筆36元的鉛筆，買3支要付多少錢？",
+        options: ["98元", "108元", "118元", "128元"],
+        correctAnswer: 1,
+      },
+    ],
+  },
+  {
+    category: "公民",
+    questions: [
+      {
+        question: "在學校遇到外國老師時，應該說什麼問候語？",
+        options: ["你好", "Hello", "再見", "晚安"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣的國花是什麼？",
+        options: ["玫瑰", "梅花", "蘭花", "杜鵑"],
+        correctAnswer: 1,
+      },
+      {
+        question: "搭乘捷運時，應該遵守哪一項規定？",
+        options: ["大聲喧嘩", "禁止飲食", "追逐奔跑", "隨地丟垃圾"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在公共場所看到垃圾時，應該怎麼做？",
+        options: ["視而不見", "撿起來丟進垃圾桶", "踢到一旁", "等別人處理"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣的消防隊電話是多少？",
+        options: ["110", "119", "113", "165"],
+        correctAnswer: 1,
+      },
+      {
+        question: "為什麼要節約用水？",
+        options: [
+          "因為水很貴",
+          "為了省錢",
+          "為了保護地球資源",
+          "因為別人這樣做",
+        ],
+        correctAnswer: 2,
+      },
+      {
+        question: "看到同學跌倒時，應該怎麼做？",
+        options: ["笑他", "馬上去扶他", "假裝沒看見", "跑走"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在路上看到紅綠燈時，應該怎麼做？",
+        options: ["快快跑過去", "等紅燈時再過", "看到綠燈才過", "不用理會"],
+        correctAnswer: 2,
+      },
+      {
+        question: "在學校發現遺失物時，應該怎麼處理？",
+        options: ["占為己有", "交給老師", "丟在原地", "送給同學"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣的國旗上有幾顆星星？",
+        options: ["12顆", "10顆", "8顆", "6顆"],
+        correctAnswer: 0,
+      },
+      {
+        question: "以下哪一個是正確的垃圾分類方式？",
+        options: [
+          "廚餘丟一般垃圾",
+          "寶特瓶回收",
+          "電池丟一般垃圾",
+          "玻璃瓶丟一般垃圾",
+        ],
+        correctAnswer: 1,
+      },
+      {
+        question: "看到老師時，應該說什麼？",
+        options: ["老師好", "嗨", "哈囉", "不用打招呼"],
+        correctAnswer: 0,
+      },
+      {
+        question: "在圖書館應該怎麼做？",
+        options: ["大聲說話", "安靜看書", "奔跑玩耍", "吃東西"],
+        correctAnswer: 1,
+      },
+      {
+        question: "坐公車時，看到老人家上車應該怎麼做？",
+        options: ["繼續坐著", "讓座給老人家", "假裝睡著", "轉頭看窗外"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在學校上課時，應該遵守什麼規定？",
+        options: ["可以睡覺", "認真聽講", "玩手機", "聊天"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣的總統任期是幾年？",
+        options: ["2年", "4年", "6年", "8年"],
+        correctAnswer: 1,
+      },
+      {
+        question: "颱風天上學時，應該攜帶什麼？",
+        options: ["游泳圈", "雨具", "玩具", "零食"],
+        correctAnswer: 1,
+      },
+      {
+        question: "如果發現同學被霸凌，應該怎麼做？",
+        options: ["視而不見", "告訴老師", "加入霸凌", "偷偷離開"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在學校午餐時間，應該注意什麼？",
+        options: [
+          "把討厭的菜丟掉",
+          "把飯菜吃完",
+          "和同學換好吃的",
+          "把飯菜倒掉",
+        ],
+        correctAnswer: 1,
+      },
+      {
+        question: "洗手的正確步驟是幾步？",
+        options: ["5步驟", "6步驟", "7步驟", "8步驟"],
+        correctAnswer: 0,
+      },
+      {
+        question: "地震時，應該優先做什麼？",
+        options: ["往外跑", "躲在桌下", "大聲尖叫", "繼續玩耍"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣的警察電話是多少？",
+        options: ["110", "119", "113", "165"],
+        correctAnswer: 0,
+      },
+      {
+        question: "如果遇到陌生人要求跟他走，應該怎麼做？",
+        options: ["跟他走", "大聲求救", "躲起來", "跑走"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在馬路上行走時，應該走在哪裡？",
+        options: ["馬路中間", "人行道", "快車道", "機車道"],
+        correctAnswer: 1,
+      },
+      {
+        question: "上廁所後應該做什麼？",
+        options: ["直接離開", "洗手", "和同學玩", "吃東西"],
+        correctAnswer: 1,
+      },
+      {
+        question: "如果發現教室有人受傷，應該怎麼做？",
+        options: ["繼續玩", "馬上告訴老師", "等下課再說", "不理會"],
+        correctAnswer: 1,
+      },
+      {
+        question: "在公車上應該注意什麼？",
+        options: ["大聲唱歌", "安靜乘坐", "追逐玩耍", "隨地丟垃圾"],
+        correctAnswer: 1,
+      },
+      {
+        question: "過馬路時，應該注意什麼？",
+        options: ["邊走邊玩手機", "遵守交通號誌", "跟著別人跑", "任意穿越"],
+        correctAnswer: 1,
+      },
+      {
+        question: "如果在路上遺失物品，應該打電話給哪裡？",
+        options: ["119", "110", "失物招領中心", "165"],
+        correctAnswer: 2,
+      },
+      {
+        question: "在公共場所大聲喧嘩是否恰當？",
+        options: ["恰當", "不恰當", "看心情", "都可以"],
+        correctAnswer: 1,
+      },
+    ],
+  },
+  {
+    category: "地理",
+    questions: [
+      {
+        question: "台灣的首都是哪一個城市？",
+        options: ["台北市", "新北市", "高雄市", "台中市"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣最高的山是什麼？",
+        options: ["阿里山", "玉山", "雪山", "大霸尖山"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣位於哪一個洲？",
+        options: ["歐洲", "美洲", "亞洲", "非洲"],
+        correctAnswer: 2,
+      },
+      {
+        question: "下列哪個不是台灣的離島？",
+        options: ["澎湖", "金門", "馬祖", "沖繩"],
+        correctAnswer: 3,
+      },
+      {
+        question: "台灣最南端的縣市是？",
+        options: ["高雄市", "屏東縣", "台東縣", "嘉義縣"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣四面環繞的是什麼？",
+        options: ["太平洋", "大西洋", "印度洋", "北冰洋"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣最大的湖泊是什麼？",
+        options: ["日月潭", "澄清湖", "鯉魚潭", "德陽湖"],
+        correctAnswer: 0,
+      },
+      {
+        question: "陽明山國家公園位於哪個縣市？",
+        options: ["新北市", "台北市", "桃園市", "基隆市"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個縣市與海洋無關？",
+        options: ["南投縣", "基隆市", "宜蘭縣", "台東縣"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣主要的氣候屬於哪一種？",
+        options: ["熱帶氣候", "溫帶氣候", "寒帶氣候", "副熱帶氣候"],
+        correctAnswer: 3,
+      },
+      {
+        question: "台灣最大的港口是？",
+        options: ["基隆港", "台中港", "高雄港", "花蓮港"],
+        correctAnswer: 2,
+      },
+      {
+        question: "阿里山最有名的自然景觀是什麼？",
+        options: ["日出", "溫泉", "瀑布", "沙灘"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣最長的河流是？",
+        options: ["淡水河", "濁水溪", "高屏溪", "大甲溪"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個縣市盛產柑橘？",
+        options: ["嘉義縣", "台南市", "新竹縣", "花蓮縣"],
+        correctAnswer: 2,
+      },
+      {
+        question: "台灣最大的平原是？",
+        options: ["蘭陽平原", "花東平原", "屏東平原", "嘉南平原"],
+        correctAnswer: 3,
+      },
+      {
+        question: "台北101位於哪個行政區？",
+        options: ["中正區", "信義區", "大安區", "松山區"],
+        correctAnswer: 1,
+      },
+      {
+        question: "日月潭位於哪個縣市？",
+        options: ["南投縣", "台中市", "嘉義縣", "雲林縣"],
+        correctAnswer: 0,
+      },
+      {
+        question: "哪個季節會有颱風來襲？",
+        options: ["春季", "夏季", "秋季", "冬季"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣冬季盛行的風是？",
+        options: ["西南風", "東北風", "南風", "北風"],
+        correctAnswer: 1,
+      },
+      {
+        question: "下列哪個地方以溫泉聞名？",
+        options: ["北投", "淡水", "八里", "三重"],
+        correctAnswer: 0,
+      },
+      {
+        question: "哪個縣市被稱為水果王國？",
+        options: ["台北市", "台中市", "高雄市", "台南市"],
+        correctAnswer: 2,
+      },
+      {
+        question: "台灣最大的平地原住民族是？",
+        options: ["阿美族", "泰雅族", "排灣族", "布農族"],
+        correctAnswer: 0,
+      },
+      {
+        question: "清境農場位於哪個縣市？",
+        options: ["南投縣", "台中市", "彰化縣", "雲林縣"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣最南端的地標是？",
+        options: ["鵝鑾鼻燈塔", "三仙台", "富貴角燈塔", "大港橋"],
+        correctAnswer: 0,
+      },
+      {
+        question: "下列哪個縣市以花卉聞名？",
+        options: ["台北市", "台中市", "彰化縣", "南投縣"],
+        correctAnswer: 2,
+      },
+      {
+        question: "台灣最大的火車站是？",
+        options: ["台北車站", "台中車站", "高雄車站", "桃園車站"],
+        correctAnswer: 0,
+      },
+      {
+        question: "哪個城市被稱為台灣的文化古都？",
+        options: ["台北", "台中", "台南", "高雄"],
+        correctAnswer: 2,
+      },
+      {
+        question: "九份位於哪個縣市？",
+        options: ["基隆市", "新北市", "台北市", "桃園市"],
+        correctAnswer: 1,
+      },
+      {
+        question: "台灣最大的森林遊樂區是？",
+        options: ["阿里山", "溪頭", "太平山", "八仙山"],
+        correctAnswer: 0,
+      },
+      {
+        question: "台灣主要的稻米產地在？",
+        options: ["宜蘭平原", "花東平原", "嘉南平原", "台北盆地"],
+        correctAnswer: 2,
+      },
+    ],
+  },
+];
